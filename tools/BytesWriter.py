9eeb24f6a7c90059a578ad682e1f407aadfd3914
toast254 <toast254@users.noreply.github.com>
2017-07-13 16:10:46 +0200

    Update BytesWriter.py

--- tools/BytesWriter.py
+++ tools/BytesWriter.py
@@ -4,10 +4,12 @@ import struct
 
 
 def write_int8(octet: bytes, pos: int, value: int):
+    octet = bytearray(octet)
     octet[pos] = value
 
 
 def write_int16(octet: bytes, pos: int, value: int, big_endian: bool = False):
+    octet = bytearray(octet)
     if big_endian:
         octet[pos] = value & 0xFF00
         octet[pos + 1] = value & 0x00FF
@@ -17,6 +19,7 @@ def write_int16(octet: bytes, pos: int, value: int, big_endian: bool = False):
 
 
 def write_int24(octet: bytes, pos: int, value: int, big_endian: bool = False):
+    octet = bytearray(octet)
     if big_endian:
         octet[pos] = value & 0xFF0000
         octet[pos + 1] = value & 0x00FF00
@@ -28,8 +31,9 @@ def write_int24(octet: bytes, pos: int, value: int, big_endian: bool = False):
 
 
 def write_int32(octet: bytes, pos: int, value: int, big_endian: bool = False):
+    octet = bytearray(octet)
     if big_endian:
-        octet[pos] = value & 0xFF000000
+        octet[pos] = byte(value & 0xFF000000)
         octet[pos + 1] = value & 0x00FF0000
         octet[pos + 2] = value & 0x0000FF00
         octet[pos + 3] = value & 0x000000FF
@@ -41,6 +45,7 @@ def write_int32(octet: bytes, pos: int, value: int, big_endian: bool = False):
 
 
 def write_str(octet: bytes, pos: int, length: int, value: str, big_endian: bool = False):
+    octet = bytearray(octet)
     if big_endian:
         for i in range(length):
             octet[pos + length - i - 1] = value[i]
