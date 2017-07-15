ca4d1c476679612071956b961ee40e31269b8b05
Tosi Émeric <emeric254@users.noreply.github.com>
2017-07-15 06:55:08 +0200

    bit shift missing

--- tools/BytesWriter.py
+++ tools/BytesWriter.py
@@ -11,36 +11,36 @@ def write_int8(octet: bytes, pos: int, value: int):
 def write_int16(octet: bytes, pos: int, value: int, big_endian: bool = False):
     octet = bytearray(octet)
     if big_endian:
-        octet[pos] = value & 0xFF00
+        octet[pos] = value >> 8
         octet[pos + 1] = value & 0x00FF
     else:
-        octet[pos + 1] = value & 0xFF00
+        octet[pos + 1] = value >> 8
         octet[pos] = value & 0x00FF
 
 
 def write_int24(octet: bytes, pos: int, value: int, big_endian: bool = False):
     octet = bytearray(octet)
     if big_endian:
-        octet[pos] = value & 0xFF0000
-        octet[pos + 1] = value & 0x00FF00
+        octet[pos] = value >> 16
+        octet[pos + 1] = value >> 8 & 0x0000FF
         octet[pos + 2] = value & 0x0000FF
     else:
-        octet[pos + 2] = value & 0xFF0000
-        octet[pos + 1] = value & 0x00FF00
+        octet[pos + 2] = value >> 16
+        octet[pos + 1] = value >> 8 & 0x0000FF
         octet[pos] = value & 0x0000FF
 
 
 def write_int32(octet: bytes, pos: int, value: int, big_endian: bool = False):
     octet = bytearray(octet)
     if big_endian:
-        octet[pos] = byte(value & 0xFF000000)
-        octet[pos + 1] = value & 0x00FF0000
-        octet[pos + 2] = value & 0x0000FF00
+        octet[pos] = value >> 24
+        octet[pos + 1] = value >> 16 & 0x0000FF
+        octet[pos + 2] = value >> 8 & 0x0000FF
         octet[pos + 3] = value & 0x000000FF
     else:
-        octet[pos + 3] = value & 0xFF000000
-        octet[pos + 2] = value & 0x00FF0000
-        octet[pos + 1] = value & 0x0000FF00
+        octet[pos + 3] = value >> 24
+        octet[pos + 2] = value >> 16 & 0x0000FF
+        octet[pos + 1] = value >> 8 & 0x0000FF
         octet[pos] = value & 0x000000FF
 
 
