aa881a37665fcdb26126e006331f150a9a25af70
emeric254 <emeric254@live.fr>
2018-12-30 15:56:46 +0100

    improve code quality, make the soft more useable

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+selected_section = None
 bios = None
 
 
@@ -16,7 +17,13 @@ def save_rom(file_name):
     if bios:
         bios.calculate_checksum()
         RomWriter.write_rom(file_name, bios.rom)
-        info_dialog = Gtk.MessageDialog(main_window, 0, Gtk.MessageType.INFO, Gtk.ButtonsType.OK, "Bios file saved")
+        info_dialog = Gtk.MessageDialog(
+            parent = main_window,
+            flags = 0,
+            message_type = Gtk.MessageType.INFO,
+            buttons = Gtk.ButtonsType.OK,
+            text = "Bios file saved"
+        )
         info_dialog.run()
         info_dialog.destroy()
 
@@ -26,32 +33,67 @@ def load_rom(file_name):
     bios = None
     rom = None
     if not file_name.endswith('.rom'):
-        error_dialog = Gtk.MessageDialog(main_window, 0, Gtk.MessageType.ERROR, Gtk.ButtonsType.OK, "Verify this file is a bios rom")
+        error_dialog = Gtk.MessageDialog(
+            parent = main_window,
+            flags = 0,
+            message_type = Gtk.MessageType.ERROR,
+            buttons = Gtk.ButtonsType.OK,
+            text = "Verify this file is a bios rom"
+        )
         error_dialog.run()
         error_dialog.destroy()
         return
     rom = RomReader.read_rom(file_name)
-    if rom:
-        bios = HawaiiBios(rom)
-        if not bios.is_supported():
-            warning_dialog = Gtk.MessageDialog(main_window, 0, Gtk.MessageType.WARNING, Gtk.ButtonsType.OK, "Please care, this device id is not listed as supported")
-            warning_dialog.run()
-            warning_dialog.destroy()
+    if not rom:
+        return
+    bios = HawaiiBios(rom)
+    if not bios.is_supported():
+        warning_dialog = Gtk.MessageDialog(
+            parent = main_window,
+            flags = 0,
+            message_type = Gtk.MessageType.WARNING,
+            buttons = Gtk.ButtonsType.OK,
+            text = "Please care, this device id is not listed as supported"
+        )
+        warning_dialog.run()
+        warning_dialog.destroy()
 
 
 def load_sections():
+    # reset selected section
+    global selected_section
+    selected_section = None
+    # reset all displayed data
     field_list_store.clear()
     section_list_store.clear()
-    if bios:
-        for key in sorted(bios.data.keys()):
-            section_list_store.append([key])
+    # ensure bios is loaded
+    if not bios:
+        return
+    # fill section displayed data
+    for key in sorted(bios.data.keys()):
+        section_list_store.append([key])
 
 
 def load_fields(section):
+    # reset diplayed data
     field_list_store.clear()
-    if bios:
-        for row in bios.data[section]:
-            field_list_store.append([row['name'], row['value'], row['unit'], row['position'], row['length']])
+    # ensure bios is loaded
+    if not bios:
+        return
+    # to know which section is displayed
+    global selected_section
+    selected_section = section
+    # add data to the displayed list from the bios decoded data
+    for row in bios.data[section]:
+        field_list_store.append(
+            [
+                row['name'],
+                row['value'],
+                row['unit'],
+                row['position'],
+                row['length']
+            ]
+        )
 
 
 class Handler:
@@ -70,18 +112,22 @@ class Handler:
 
     @staticmethod
     def onEditValue(cell, path, new_value):
+        # update the bios decoded data and displayed data
+        bios.data[selected_section][int(path)]['value'] = new_value
         field_list_store[path][1] = new_value
+        # get value position and length
         pos = int(field_list_store[path][3], 0)
         length = field_list_store[path][4]
+        # write the value into the RAM loaded bios hex data
         if length == '8 bits':
             bios.rom = BytesWriter.write_int8(bios.rom, pos, int(new_value))
-        if length == '16 bits':
+        elif length == '16 bits':
             bios.rom = BytesWriter.write_int16(bios.rom, pos, int(new_value))
-        if length == '24 bits':
+        elif length == '24 bits':
             bios.rom = BytesWriter.write_int24(bios.rom, pos, int(new_value))
-        if length == '32 bits':
+        elif length == '32 bits':
             bios.rom = BytesWriter.write_int32(bios.rom, pos, int(new_value))
-        if length.endswith('chars'):
+        elif length.endswith('chars'):
             bios.rom = BytesWriter.write_str(bios.rom, pos, int(length.split(' ')[0]), new_value)
 
     @staticmethod
@@ -100,7 +146,12 @@ class Handler:
         filter_any.set_name("Any file")
         filter_any.add_pattern("*")
         #
-        dialog = Gtk.FileChooserDialog("Open bios file", main_window, Gtk.FileChooserAction.OPEN, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+        dialog = Gtk.FileChooserDialog(
+            title = "Open bios file",
+            parent = main_window,
+            action = Gtk.FileChooserAction.OPEN,
+            buttons = (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK)
+        )
         dialog.add_filter(filter_bios_rom)
         dialog.add_filter(filter_any)
         try:
@@ -115,7 +166,12 @@ class Handler:
     @staticmethod
     def onSaveFile(*args):
         try:
-            dialog = Gtk.FileChooserDialog("Save bios file", main_window, Gtk.FileChooserAction.SAVE, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+            dialog = Gtk.FileChooserDialog(
+                title = "Save bios file",
+                parent = main_window,
+                action = Gtk.FileChooserAction.SAVE,
+                buttons = (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK)
+            )
             response = dialog.run()
             if response == Gtk.ResponseType.OK:
                 file_name = dialog.get_filename()
